5a60f119eb3c4b58035fc34379d8925d5d8007ea
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-10-09 08:38:02 +0200

    Tailing list

--- src/main/scala/com/kbhit/playground/scala/CustomList.scala
+++ src/main/scala/com/kbhit/playground/scala/CustomList.scala
@@ -18,6 +18,11 @@ object CustomList {
     case Cons(x, xs) => x * product(xs)
   }
 
+  def tail[A](list: CustomList[A], count: Int = 1): CustomList[A] = (list, count) match {
+    case (Cons(_, xs), _) if count > 0 => tail(xs, count - 1)
+    case _ => list
+  }
+
   def apply[A](as: A*): CustomList[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
--- src/test/scala/com/kbhit/playground/scala/CustomListTest.scala
+++ src/test/scala/com/kbhit/playground/scala/CustomListTest.scala
@@ -8,4 +8,12 @@ class CustomListTest extends FlatSpec with Matchers {
     CustomList.sum(CustomList[Int](1,2)) should be (3)
   }
 
+  "tail of [1,2]" should "be [2]" in {
+    CustomList.tail(CustomList[Int](1,2)) should be (CustomList[Int](2))
+  }
+
+  "tail 2 of [1,2,3,4]" should "be [3,4]" in {
+    CustomList.tail(CustomList[Int](1,2,3,4), 2) should be (CustomList[Int](3,4))
+  }
+
 }
