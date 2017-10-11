96ab3ef69a445d716449eab217f4a31dd217ab86
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-10-11 20:47:13 +0200

    Using fold for all methods in tree

--- src/main/scala/com/kbhit/playground/scala/CustomTree.scala
+++ src/main/scala/com/kbhit/playground/scala/CustomTree.scala
@@ -7,31 +7,27 @@ case class Branch[+A](left: CustomTree[A], right: CustomTree[A]) extends CustomT
 
 object CustomTree {
 
-  def countLeaves[A](t: CustomTree[A]): Int = t match {
-    case null => 0
-    case Branch(left, right) => countLeaves(left) + countLeaves(right)
-    case Leaf(_) => 1
-  }
+  def countLeaves[A](t: CustomTree[A]): Int =
+    fold(t, 0)((x, y) => x + y, _ => 1)
 
-  def maximum(t: CustomTree[Int]): Int = t match {
-    case Branch(x, y) => maximum(x) max maximum(y)
-    case Leaf(x) => x
-    case null => Int.MinValue
-  }
+  def maximum(t: CustomTree[Int]): Int =
+    fold(t, Int.MinValue)((x, y) => x max y, x => x)
 
   def depth[A](t: CustomTree[A]): Int =
-    fold(t, 0)((x, y) => (x max y) + 1)
+    fold(t, 0)((x, y) => (x max y) + 1, _ => 1)
 
-  def map[A,B](t: CustomTree[A])(f: A => B): CustomTree[B] = t match {
-    case Branch(x, y) => Branch(map(x)(f), map(y)(f))
-    case Leaf(x) => Leaf(f(x))
-    case _ => null
-  }
+  def map[A,B](t: CustomTree[A])(f: A => B): CustomTree[B] =
+    fold(t, CustomTree[B]())((x, y) => Branch(x, y), x => Leaf(f(x)))
 
-  def fold[A,B](t: CustomTree[A], z: B)(f: (B, B) => B): B = t match {
-    case Branch(x, y) => f(fold(x, z)(f), fold(y, z)(f))
-    case Leaf(_) => f(z, z)
+  def fold[A,B](t: CustomTree[A], z: B)(f: (B, B) => B, g: A => B): B = t match {
+    case Branch(x, y) => f(fold(x, z)(f, g), fold(y, z)(f, g))
+    case Leaf(x) => g(x)
     case _ => z
   }
+
+  def apply[A](as: A*): CustomTree[A] =
+    if (as.isEmpty) null
+    else if (as.size == 1) Leaf(as.head)
+    else Branch(apply(as.head), apply(as.last))
 
 }
