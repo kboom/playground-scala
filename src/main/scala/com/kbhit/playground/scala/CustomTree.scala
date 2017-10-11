9d176c643327da7c0d43731b9c6256cc618bdbd6
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-10-11 20:17:03 +0200

    Depth using fold

--- src/main/scala/com/kbhit/playground/scala/CustomTree.scala
+++ src/main/scala/com/kbhit/playground/scala/CustomTree.scala
@@ -19,16 +19,19 @@ object CustomTree {
     case null => Int.MinValue
   }
 
-  def depth[A](t: CustomTree[A]): Int = t match {
-    case Branch(x, y) => (depth(x) max depth(y)) + 1
-    case null => 0
-    case Leaf(_) => 1
-  }
+  def depth[A](t: CustomTree[A]): Int =
+    fold(t, 0)((x, y) => (x max y) + 1)
 
-  def map[A, B](t: CustomTree[A])(f: A => B): CustomTree[B] = t match {
+  def map[A,B](t: CustomTree[A])(f: A => B): CustomTree[B] = t match {
     case Branch(x, y) => Branch(map(x)(f), map(y)(f))
     case Leaf(x) => Leaf(f(x))
     case _ => null
+  }
+
+  def fold[A,B](t: CustomTree[A], z: B)(f: (B, B) => B): B = t match {
+    case Branch(x, y) => f(fold(x, z)(f), fold(y, z)(f))
+    case Leaf(_) => f(z, z)
+    case _ => z
   }
 
 }
